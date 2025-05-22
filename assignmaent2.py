3edcb6d64ba389fac72b4987b78f011e7ca9ffdb
chamanbaman80 <fatemagangardi9633@gmail.com>
2025-05-22 13:30:36 +0530

    Add files via upload

---
+++ assignmaent2.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a number:"))
+if a%2==1:
+    print("The number is odd.")
+else:
+    print("The number is even.")
+
---
+++ assignmenf 2 part 2.py
@@ -0,0 +1,4 @@
+n=sum(range(1,51,1))
+for i in range(1, 51):
+    print(i)
+print("The sum of 1 to 50 is", n)   
